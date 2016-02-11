cedb14fbf7e53bfaa4c9aa05ea0d19e4b888373c
Tim Dang <timothy.dang@gmail.com>
2016-02-11 10:37:25 -0800

    grunt automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
-      // add your production server task here
+      grunt.task.run(['deploy']);   // add your production server task here
 
     } else {
       grunt.task.run([ 'server-dev' ]);
@@ -126,7 +126,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('deploy', [
-
+    'test', 'build'
   ]);
 
 
